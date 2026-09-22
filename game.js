d67471e1c6d4c95dd45621946a72a075fae294d4
MofMof-Inu <forbackupfrom2018@gmail.com>
2026-09-23 02:02:55 +0900

    Update game.js

--- game.js
+++ game.js
@@ -14,7 +14,7 @@
 // 1. ゲームの基本設定
 // ============================================================
 
-const GAME_VERSION = "v0.1.4";
+const GAME_VERSION = "v0.1.5";
 const GAME_CONFIG = {
   // Canvasの大きさ
   width: 800,
