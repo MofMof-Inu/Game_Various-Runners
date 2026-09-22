320f00078ccca85304f3b5231a692384688d0aea
MofMof-Inu <forbackupfrom2018@gmail.com>
2026-09-22 23:33:10 +0900

    Update game.js

--- game.js
+++ game.js
@@ -30,7 +30,7 @@ const GAME_CONFIG = {
   speedIncrease: 0.0005,
 
   // 重力
-  gravity: 0.9,
+  gravity: 1,
 
   // ジャンプの強さ
   jumpPower: -14,
@@ -345,8 +345,8 @@ function isColliding(a, b) {
   const playerMarginY = 20;
 
   // 障害物側の当たり判定
-  const obstacleMarginX = 30;
-  const obstacleMarginY = 30;
+  const obstacleMarginX = 45;
+  const obstacleMarginY = 45;
 
   return (
     a.x + playerMarginX < b.x + b.width - obstacleMarginX &&
