6b84c53b43c0a6805b159ee4276629f90f0e55ef
MofMof-Inu <forbackupfrom2018@gmail.com>
2026-09-25 22:50:48 +0900

    Fix GAME_VERSION string syntax

--- game.js
+++ game.js
@@ -14,7 +14,7 @@
 // 1. ゲームの基本設定
 // ============================================================
 
-const GAME_VERSION = "v0.1.61;
+const GAME_VERSION = "v0.1.61";
 const GAME_CONFIG = {
   // Canvasの大きさ
   width: 800,
